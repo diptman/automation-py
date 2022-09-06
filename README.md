efb9fd83ea2b1589ef8e54ec2a3b5b5fd115a556
diptman <diptmangupta.j@gmail.com>
2022-09-07 01:21:11 +0530

    Basic Structure of project

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # automation-py
 python , pytest  and selenium automation framework
+### Pre-requisites
+1. Python is installed
+
+### Steps to execute the code on windows 
+1. Go to root directory (automation-py)
+2. Create virtual environment using command : `python -m venv venv`
+3. Activate virtual environment : `.\venv\Script\activate`
+4. Install required dependency in your venv : `pip install -r requirements.txt`
+5. To run the tests : `python -m pytest`
+6. To deactivate the virtual environment : `deactivate`
---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+import logging
+
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+
+@pytest.fixture()
+def browser_type(browser):
+    if browser == 'chrome':
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+    elif browser == 'firefox':
+        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    else:
+        logging.critical("# Invalid browser argument passed ", browser)
+    driver.implicitly_wait(10)
+    return driver
+    # Similarly we can add all required browsers here.
+
+
+# for running test on specific browser user needs to pass argurment as --browser="browser_name"
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", default='chrome')
+
+
+# return browser to setup method
+@pytest.fixture
+def browser(request):
+    return request.config.getoption("--browser")

e8fb3684e09c03ca1e607f0d7aa79a7e6d7fb74d
diptman <diptmangupta.j@gmail.com>
2022-09-08 09:34:50 +0530

    Selecting Recharge Amount

--- pages/page_phone_recharge.py
+++ pages/page_phone_recharge.py
@@ -6,9 +6,10 @@ class PhoneRecharge:
     _name_mobile_number_input = "mobile"
     _name_operator_input = "operator"
     _css_operator_name_dropdown = "li[data-show='{}']"
+    _name_amount_input = "amount"
+    _xpath_recharge_amount_value ="(//*[@class='main-info data-value']/b[text() ='{}'])[1]"
 
     # functions
-
     def __init__(self, driver):
         self.driver = driver
 
@@ -16,4 +17,11 @@ class PhoneRecharge:
         self.driver.find_element(By.CSS_SELECTOR, self._css_operator_name_dropdown.format(operator_name)).click()
 
     def open_operator_input(self):
-        self.driver.find_element(By.NAME,self._name_operator_input).click();
+        self.driver.find_element(By.NAME,self._name_operator_input).click()
+
+    def enter_phone_number(self, phone_number):
+        self.driver.find_element(By.NAME, self._name_mobile_number_input).send_keys(phone_number)
+
+    def enter_the_recharge_amount(self, amount):
+        self.driver.find_element(By.NAME, self._name_amount_input).click()
+        self.driver.find_element(By.XPATH, self._xpath_recharge_amount_value.format(amount)).click()
--- tests/test_recharge_mobile.py
+++ tests/test_recharge_mobile.py
@@ -1,5 +1,5 @@
 import configparser
-
+import logging
 from pages.page_phone_recharge import PhoneRecharge
 
 config = configparser.ConfigParser()
@@ -7,11 +7,17 @@ config.read('config.ini')
 
 operator = config.get("USER_DATA", "operator")
 phone_number = config.get("USER_DATA", "phone")
+recharge_amount = config.get("USER_DATA", "amount")
 
 class TestMobileRecharge:
 
     def test_recharge_mobile_phone(self,browser_type):
         self.driver = browser_type
         phone_recharge = PhoneRecharge(self.driver)
+        logging.info("# Enter Phone Number")
+        phone_recharge.enter_phone_number(phone_number)
+        logging.info("# Select the operator from Drop down")
         phone_recharge.open_operator_input()
         phone_recharge.select_operator(operator)
+        logging.info("# Select Recharge Amount")
+        phone_recharge.enter_the_recharge_amount(recharge_amount)
